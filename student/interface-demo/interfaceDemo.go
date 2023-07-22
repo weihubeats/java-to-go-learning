e1414353aff49a359c1f6ff46b1c62f86b43b22d
weihu <weihu@apache.org>
2023-07-22 20:06:44 +0800

    add interfaceDemo.go

--- student/interface-demo/interfaceDemo.go
+++ student/interface-demo/interfaceDemo.go
@@ -1,11 +1,32 @@
-package interface_demo
+package main
 
-type Student interface {
+import "fmt"
+
+type Fruit interface {
 
 	//方法
 	SetName(name string) bool
 }
 
-func main() {
+type Apple struct {
+	Name string
+}
 
+// 实现了 Fruit 接口
+func (xiaozou *Apple) SetName(name string) bool {
+	xiaozou.Name = name
+	return true
+}
+
+func main() {
+	var apple Apple
+	// 给接口赋值
+	var fruit Fruit = &apple
+	fruit.SetName("苹果")
+	fmt.Println("name ", apple.Name)
+
+	// 判断接口是否为 Apple 类型
+	if aa, ok := fruit.(*Apple); ok {
+		fmt.Println(aa.Name)
+	}
 }
