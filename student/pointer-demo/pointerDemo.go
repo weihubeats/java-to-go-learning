2049b41d9e9706697288222681e5943d9db01683
weihu <weihu@apache.org>
2023-08-07 19:48:34 +0800

    add rocketmq producer

--- student/pointer-demo/pointerDemo.go
+++ student/pointer-demo/pointerDemo.go
@@ -18,4 +18,20 @@ func main() {
 	// update xiao zou
 	fmt.Println(zou)
 
+	x, y := 1, 2
+	// 交换变量值
+	swap(&x, &y)
+	// 输出变量值
+	fmt.Println(x, y)
+
+}
+
+// 交换函数
+func swap(a, b *int) {
+	// 取a指针的值, 赋给临时变量t
+	t := *a
+	// 取b指针的值, 赋给a指针指向的变量
+	*a = *b
+	// 将a指针的值赋给b指针指向的变量
+	*b = t
 }
